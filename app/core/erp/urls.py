9854803d724f2f0b51435e21ee436efc3791578e
Aleja <afajardo04@misena.edu.co>
2023-07-05 15:49:35 -0500

    mi primer listview categoria

--- app/core/erp/urls.py
+++ app/core/erp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from core.erp.views.categoria.views import *
 
 urlpatterns = [
-  path('categoria/list/', categoria_list, name='categoria_list'),
+  path('categoria/list/', CategoriaListView.as_view(), name='categoria_list'),
 ]        
--- app/core/erp/views/categoria/views.py
+++ app/core/erp/views/categoria/views.py
@@ -1,6 +1,21 @@
+from typing import Any, Dict
 from django.shortcuts import render
+from core.erp.models import *
+from django.views.generic import *
+
 def categoria_list(request):
     data={
-
+        'title':'Listado de categorias',
+        'categorias':Categoria.objects.all()
     }
     return render(request,'categoria/list.html',data)
+
+class CategoriaListView(ListView):
+    model=Categoria
+    template_name='categoria/list.html'
+
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context['title']='Listado de categorias'
+        #context['object_list']=Producto.objects.all()
+        return context
