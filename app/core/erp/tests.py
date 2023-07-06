4439575ae47c6220023d3563ddf84d728a06b4c3
alejandra <afajardo04@misena.edu.co>
2023-07-06 12:43:20 -0500

    ListView implementando datatables

--- app/core/erp/tests.py
+++ app/core/erp/tests.py
@@ -1,24 +1,11 @@
 from config.wsgi import *
 from core.erp.models import *
-#from django.test import TestCase
 
-#Insertar
-#valores=["Lacteos","Cereales","Carnes"]
-#for valor in valores:
-  #c=Categoria()
-  #Porque estoy recorriendo el arreglo
-  #c.Nombre=valor
-  #c.save()
-  
-#LISTAR
-#print(Categoria.objects.all())
-#for i in Categoria.objects.filter():
-    #print(i)
+data = ['Leche y derivados', 'Carnes, pescados y huevos', 'Patatas, legumbres, frutos secos',
+        'Verduras y Hortalizas', 'Frutas', 'Cereales y derivados, azúcar y dulces',
+        'Grasas, aceite y mantequilla']
 
-valores=[["Yogurt",2],["Arroz",3],["Pechuga de pollo",4]]
-for valor in valores: 
-  p=Producto()
-  #Porque estoy recorriendo el arreglo
-  p.Nombre=valor[0]
-  p.cate_id=valor[1]
-  p.save()
+for i in data:
+    cat = Categoria(Nombre=i)
+    cat.save()
+    print('Guardado registro N°{}'.format(cat.id))
