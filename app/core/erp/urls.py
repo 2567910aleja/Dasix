6e4cd1fb1f461aba96e6c7a58c41967841fea3c0
alejandra <afajardo04@misena.edu.co>
2023-07-06 22:34:25 -0500

    implementando decoradores

--- app/core/erp/urls.py
+++ app/core/erp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from core.erp.views.categoria.views import *
 
+app_name = 'erp'
+
 urlpatterns = [
   path('categoria/list/', CategoriaListView.as_view(), name='categoria_list'),
+  path('categoria/list2/', categoria_list, name='categoria_list2'),
 ]        
--- app/core/erp/views/categoria/views.py
+++ app/core/erp/views/categoria/views.py
@@ -1,7 +1,8 @@
-from typing import Any, Dict
-from django.shortcuts import render
+from django.contrib.auth.decorators import *
+from django.shortcuts import render, redirect
 from core.erp.models import *
 from django.views.generic import *
+from django.utils.decorators import method_decorator
 
 def categoria_list(request):
     data={
@@ -13,6 +14,11 @@ def categoria_list(request):
 class CategoriaListView(ListView):
     model=Categoria
     template_name='categoria/list.html'
+
+    #@method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
 
     def get_context_data(self, **kwargs):
         context=super().get_context_data(**kwargs)
