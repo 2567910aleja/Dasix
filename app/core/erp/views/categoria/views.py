a47febcf4b3284453ec411e72724716931b511be
alejandra <afajardo04@misena.edu.co>
2023-07-15 17:19:31 -0500

    createView - implementado ajax para envio de datos - finalizado

--- app/core/erp/views/categoria/views.py
+++ app/core/erp/views/categoria/views.py
@@ -51,16 +51,17 @@ class CategoriaCreateView(CreateView):
     template_name='Categoria/create.html'
     success_url=reverse_lazy('erp:categoria_list')
 
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
     def post(self, request, *args, **kwargs):
         data={}
         try:
             action=request.POST['action']
             if action == 'add':
                 form=self.get_form()
-                if form.is_valid():
-                    form.save()
-                else:
-                    data['error']=form.errors
+                data=form.save()
             else:
                 data['error']='No ha ingresado a ninguna opcion'
             data= Categoria.objects.get(pk=request.POST['id']).toJSON()
@@ -68,6 +69,16 @@ class CategoriaCreateView(CreateView):
             data['error']=str (e)
 
         return JsonResponse(data)
+
+    #     print(request.POST)
+    #     form = CategoriaForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return HttpResponseRedirect(self.success_url)
+    #     self.object = None
+    #     context = self.get_context_data(**kwargs)
+    #     context['form'] = form
+    #     return render(request, self.template_name, context
 
     def get_context_data(self, **kwargs):
         context=super().get_context_data(**kwargs)
