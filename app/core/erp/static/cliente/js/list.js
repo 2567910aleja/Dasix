3198e6687eabb3ac8ecfa6b52ff2e055ec44eec5
alejandra <afajardo04@misena.edu.co>
2023-07-31 21:18:02 -0500

    Modal -  creando un registro

--- app/core/erp/static/cliente/js/list.js
+++ app/core/erp/static/cliente/js/list.js
@@ -1,45 +1,50 @@
-$(function () {
-    $('#data').DataTable({
-        responsive: true,
-        autoWidth: false,
-        destroy: true,
-        deferRender: true,
-        ajax: {
-            url: window.location.pathname,
-            type: 'POST',
-            data: {
-                'action': 'searchdata'
+function getData(){
+    $(function () {
+        $('#data').DataTable({
+            responsive: true,
+            autoWidth: false,
+            destroy: true,
+            deferRender: true,
+            ajax: {
+                url: window.location.pathname,
+                type: 'POST',
+                data: {
+                    'action': 'searchdata'
+                },
+                dataSrc: ""
             },
-            dataSrc: ""
-        },
-        columns: [
-            {"data": "id"},
-            {"data": "Nombres"},
-            {"data": "Apellidos"},
-            {"data": "Cedula"},
-            {"data": "Cumple"},
-            {"data": "Sexo"},
-            {"data": "id"},
-        ],
-        columnDefs: [
-            {
-                targets: [-1],
-                class: 'text-center',
-                orderable: false,
-                render: function (data, type, row) {
-                    var buttons = '<a href="#" class="btn btn-primary btn-xs btn-flat"><i class="fas fa-edit"></i></a> ';
-                    buttons += '<a href="#" type="button" class="btn btn-danger btn-xs btn-flat"><i class="fas fa-trash-alt"></i></a>';
-                    return buttons;
-                }
-            },
-        ],
-        initComplete: function (settings, json) {
-
-        }
-    });
-
-    $('btnAdd').on('click', function(){
-        $('input[name="name"]').val('add');
+            columns: [
+                {"data": "id"},
+                {"data": "Nombres"},
+                {"data": "Apellidos"},
+                {"data": "Cedula"},
+                {"data": "Cumple"},
+                {"data": "Sexo"},
+                {"data": "id"},
+            ],
+            columnDefs: [
+                {
+                    targets: [-1],
+                    class: 'text-center',
+                    orderable: false,
+                    render: function (data, type, row) {
+                        var buttons = '<a href="#" class="btn btn-primary btn-xs btn-flat"><i class="fas fa-edit"></i></a> ';
+                        buttons += '<a href="#" type="button" class="btn btn-danger btn-xs btn-flat"><i class="fas fa-trash-alt"></i></a>';
+                        return buttons;
+                    }
+                },
+            ],
+            initComplete: function (settings, json) {
+    
+            }
+        });
+    })
+}
+    
+    $(function(){
+        getData();
+    $('.btnAdd').on('click', function(){
+        $('input[name="action"]').val('add');
         $('#myModalCliente').modal('show');
     });
 
@@ -48,7 +53,8 @@ $(function () {
         //var parametros = $(this).serializeArray();
         var parametros = new FormData(this);
         submit_with_ajax(window.location.pathname,'Guardar', '¿Quiere realizar esta accion?', parametros, function (){
-          location.reload();
+            $('#myModalCliente').modal('hide');
+            getData();
         });
-      })
-});
+      });
+    });
