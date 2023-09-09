7a73acc904ac4042051d2c3fd49bd2aadf378472
alejandra <afajardo04@misena.edu.co>
2023-09-08 19:55:30 -0500

    grupos en django

--- core/user/forms.py
+++ core/user/forms.py
@@ -10,7 +10,7 @@ class UserForm(ModelForm):
 
     class Meta:
         model = User
-        fields = 'first_name', 'last_name', 'email', 'username', 'password', 'image'
+        fields = 'first_name', 'last_name', 'email', 'username', 'password', 'image','groups'
         widgets = {
             'first_name': TextInput(
                 attrs={
@@ -37,8 +37,13 @@ class UserForm(ModelForm):
                     'placeholder': 'Ingrese su contraseña',
                 }
             ),
+            'groups':SelectMultiple(attrs={
+                'class':'form-control select2',
+                'style': 'width 100%',
+                'multiple':'multiple'
+            })
         }
-        exclude = ['groups', 'user_permissions', 'last_login', 'date_joined', 'is_superuser', 'is_active', 'is_staff']
+        exclude = ['user_permissions', 'last_login', 'date_joined', 'is_superuser', 'is_active', 'is_staff']
 
 
     def save(self, commit=True):
@@ -46,7 +51,18 @@ class UserForm(ModelForm):
         form = super()
         try:
             if form.is_valid():
-                form.save()
+                print(self.cleaned_data['groups'])
+                pwd=self.cleaned_data['password']
+                u= form.save(commit=False)
+                if u.pk is None:
+                    u.set_password(pwd)
+                else:
+                    user=User.objects.get(pk=u.pk)
+                    if user.password != pwd:
+                        u.set_password(pwd)
+                u.save()
+                for g in self.cleaned_data['groups']:
+                    u.groups.add(g)
             else:
                 data['error'] = form.errors
         except Exception as e:
--- core/user/models.py
+++ core/user/models.py
@@ -29,12 +29,3 @@ class User(AbstractUser):
         item['Date_joined'] = self.date_joined.strftime('%Y-%m-%d')
         item['image'] = self.get_image()
         return item
-
-    def save(self, *args, **kwargs):
-        if self.pk is None:
-            self.set_password(self.password)
-        else:
-            user=User.objects.get(pk=self.pk)
-            if user.password != self.password:
-                self.set_password(self.password)
-        super().save(*args, **kwargs)
